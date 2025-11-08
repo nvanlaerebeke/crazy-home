989594af4c12918c94eda9b087be70725b3f7004
Nico Van Laerebeke <nico.vanlaerebeke@gmail.com>
2025-11-08 17:49:18 +0100

    Started adding theming

---
+++ src/Home.Api/Controllers/MobileController.cs
@@ -0,0 +1,19 @@
+﻿using Home.Api.Objects.Mobile;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Home.Api.Controllers;
+
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+[ApiController]
+[Route("[controller]")]
+public sealed class MobileController : ControllerBase{
+    [HttpGet("[action]")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Theme))]
+    public async Task<IActionResult> Theme() {
+        return Ok();
+    }
+}
+
---
+++ src/Home.Api/Objects/Mobile/Theme.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Home.Api.Objects.Mobile;
+
+internal sealed class Theme {
+    [JsonPropertyName("BackgroundImage")]
+    public required string BackgroundImage { get; init; }
+    [JsonPropertyName("BackgroundColor")]
+    public required string BackgroundColor { get; init; }
+}
+
